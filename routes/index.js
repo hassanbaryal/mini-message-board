8c86a3acff1225f210a46c170ef9c59cf805d68b
hassanbaryal <h.baryal2@gmail.com>
2023-01-26 22:25:15 -0500

    Added http requests for / and /new

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hi World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Express', messages });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form', { title: 'Create New Message' });
+});
+
+router.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
